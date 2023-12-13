0f58f01d53c0dda81035dc84b9f2aad421ddd0fe
myszjordjot <116115347+myszjordjot@users.noreply.github.com>
2023-12-13 12:24:23 +0100

    add day 13

---
+++ 2023/13/13_ab.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def find_mirror(matrix, num_diff):
+    for i in range(len(matrix[0])):
+        low_bound = max(0, 2 *(i+1)-len(matrix[0]))
+        flag = False
+        if np.sum(matrix[:,low_bound:i + 1] != matrix[:,i + 1:2*i - low_bound + 2][::, ::-1] ) == num_diff and matrix[:,low_bound:i + 1].shape[1] > 0:
+            flag = True
+        if flag:
+            return i
+
+def task(inputs, task_num):
+    score = 0
+    for matrix in inputs:
+        matrix = np.array(matrix)
+        mirror_position = find_mirror(matrix, task_num)
+        if mirror_position is not None:
+            score += mirror_position + 1
+        else:
+            mirror_position = find_mirror(matrix.T, task_num)
+            score += (mirror_position + 1) * 100
+    print(f'Score: {score}')
+
+def main():
+    lines = [list(line.rstrip()) for line in open('2023/13/input.txt')]
+    inputs = []
+    current = []
+    for line in lines:
+        if line == []:
+            inputs.append(current)
+            current = []
+            continue
+        current.append(line)
+    inputs.append(current)
+
+    task(inputs, 0)
+    task(inputs, 1)
+
+if __name__ == '__main__':
+    main()
